Correcting insurance calculation, adding new parameters

SimulationCredit.py
@@ -1,17 +1,6 @@
 import numpy as np
 
-prix_appartement = 154000
-frais_de_notaire = 13200 # TODO:Check amount - find exact one
-travaux = 0
-cout_total_assurance = 0
-echeance_mensuelle_credit = 0
-cout_credit_global = 0
-capital_restant = 0
 taux_annuel_range = [0.012, 0.019]
-travaux_inclus = False
-apport_inclus = False
-cent_dix_pourcent = False
-
 loan_structure = {}
 
 def montant_credit():
@@ -101,6 +90,47 @@ def pretty_print(i, capital_restant, montant, amortissement, interets, assurance
     print(f"{i:8d}\t |  {capital_restant:18.2f} \t | {montant:10.2f} \t | {amortissement:13.2f} \t "\
           f"| {interets:8.2f} \t | {assurance:9.2f} \t | {(montant + assurance):25.2f}")
 
+def conditions_initiales():
+    # TODO: store away the initial values
+    global prix_appartement
+    global frais_de_notaire
+    global travaux
+    global cout_total_assurance
+    global echeance_mensuelle_credit
+    global cout_credit_global
+    global capital_restant
+    global apport   
+    prix_appartement = 154000
+    frais_de_notaire = 13200 # TODO:Check amount - find exact one
+    travaux = 20000
+    cout_total_assurance = 0
+    echeance_mensuelle_credit = 0
+    cout_credit_global = 0
+    capital_restant = 0
+    apport = 20000
+
+def moulinette():
+    global prix_appartement
+    global frais_de_notaire
+    global travaux
+    global cout_total_assurance
+    global echeance_mensuelle_credit
+    global cout_credit_global
+    global capital_restant    
+    taux_testes = np.arange(start=taux_annuel_range[0], stop=taux_annuel_range[1], step=0.0005)
+
+    for taux_annuel in taux_testes:
+        conditions_initiales() 
+        echeance_mensuelle_credit = echeance_mensuelle(montant_credit(), taux_annuel, duree_credit)
+        print(f"Taux d'intérêt testé : {taux_annuel*100:.2f}%")
+        
+        for i in range(1, duree_credit * int(12/frequence_echeances) +1):
+            capital_restant, montant, amortissement, interets, assurance, cout_total_assurance = calcul_multiples_echeances(capital_restant, taux_annuel, frequence_echeances, info_assurance, assurance_degressive, duree_credit)
+        
+            if tableau_amortissement:
+                pretty_print(i, capital_restant, montant, amortissement, interets, assurance)
+        
+        print(f"Cout total de l'assurance : {cout_total_assurance:.2f}, cout total du credit : {cout_credit_global:.2f}, montant de l'echeance : {echeance_mensuelle_credit:.2f}\n")
 
 if __name__ == "__main__":
 
@@ -115,7 +145,7 @@ if __name__ == "__main__":
     frequence_echeances = 1
     assurance_degressive = True
     info_assurance = 0.0035
-    tableau_amortissement = False
+    tableau_amortissement = False 
     i = 0
 
     if frequence_echeances == 0:
@@ -125,16 +155,24 @@ if __name__ == "__main__":
     if frequence_echeances < 0:
         raise ValueError("Bien tenté mais non")
 
-    taux_testes = np.arange(start=taux_annuel_range[0], stop=taux_annuel_range[1], step=0.0005)
+    
+    print(" ========================= Test 1 : sans travaux, ni apport, ni 110%") 
+    travaux_inclus = False
+    apport_inclus = False
+    cent_dix_pourcent = False
 
-    for taux_annuel in taux_testes:
-        echeance_mensuelle_credit = echeance_mensuelle(montant_credit(), taux_annuel, duree_credit)
-        print(f"Taux d'intérêt testé : {taux_annuel*100:.2f}%")
-        
-        for i in range(1, duree_credit * int(12/frequence_echeances) +1):
-            capital_restant, montant, amortissement, interets, assurance, cout_total_assurance = calcul_multiples_echeances(capital_restant, taux_annuel, frequence_echeances, info_assurance, assurance_degressive, duree_credit)
-        
-        if tableau_amortissement:
-                pretty_print(i, capital_restant, montant, amortissement, interets, assurance)
-        
-        print(f"Cout total de l'assurance : {cout_total_assurance:.2f}, cout total du credit : {cout_credit_global:.2f}, montant de l'echeance : {echeance_mensuelle_credit:.2f}")
+    moulinette()
+
+    print("========================= Test 2 : avec travaux, sans apport, avec 110%") 
+    travaux_inclus = True
+    apport_inclus = False
+    cent_dix_pourcent = True
+
+    moulinette()
+    
+    print("========================= Test 3 : avec travaux, avec apport, avec 110%") 
+    travaux_inclus = True
+    apport_inclus = True
+    cent_dix_pourcent = True
+
+    moulinette()